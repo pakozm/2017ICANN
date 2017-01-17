111ce5e67121d8120f8752d296a50053fe0af01a
Paco Zamora Martinez <pakozm@gmail.com>
2017-01-17 23:16:31 +0100

    More debug

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM gcr.io/tensorflow/tensorflow:latest-gpu
-RUN pip install keras
+RUN pip install keras pandas
 # RUN apt-get update && apt-get install nvidia-modprobe && rm -Rf /var/
--- fmloss/fmloss.py
+++ fmloss/fmloss.py
@@ -1,26 +1,31 @@
-
 '''Trains a simple convnet on the MNIST dataset.
 Gets to 99.25% test accuracy after 12 epochs
 (there is still a lot of margin for parameter tuning).
 16 seconds per epoch on a GRID K520 GPU.
 '''
-
 from __future__ import print_function
+
+import sys
 import numpy as np
 np.random.seed(1337)  # for reproducibility
 
+from keras.callbacks import EarlyStopping
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
+from keras.regularizers import l2, activity_l2
 from keras.utils import np_utils
 from keras import backend as K
+
+sys.path.append('.')
+from datasets import bioassay as ds_module
 
 def macro_avg_fm_loss(beta=1.0):
     def macro_fm(y_true, y_pred):
         beta2 = beta**2.0
         top = K.sum(y_true * y_pred, axis=0)
-        bot = K.sum(y_true, axis=0) + beta2 * K.sum(y_pred, axis=0)
+        bot = beta2 * K.sum(y_true, axis=0) + K.sum(y_pred, axis=0)
         return -(1.0 + beta2) * K.mean(top/bot)
     return macro_fm
 
@@ -32,71 +37,80 @@ def micro_avg_fm_loss(beta=1.0):
     def micro_fm(y_true, y_pred):
         beta2 = beta**2.0
         top = K.sum(y_true * y_pred)
-        bot = K.sum(y_true) + beta2 * K.sum(y_pred)
+        bot = beta2 * K.sum(y_true) + K.sum(y_pred)
         return -(1.0 + beta2) * top / bot
     return micro_fm
 
-batch_size = 128
-nb_classes = 10
-nb_epoch = 20
-
-# input image dimensions
-img_rows, img_cols = 28, 28
-# number of convolutional filters to use
-nb_filters = 32
-# size of pooling area for max pooling
-pool_size = (2, 2)
-# convolution kernel size
-kernel_size = (3, 3)
-
 # the data, shuffled and split between train and test sets
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
+(X_train, Y_train), (X_test, Y_test) = ds_module.load_data(idx=2)
 
-if K.image_dim_ordering() == 'th':
-    X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
-    X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
-else:
-    X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
-    X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
+batch_size = 128
+nb_classes = len(np.unique(Y_train))
+nb_epoch = 200
 
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
-X_train /= 255
-X_test /= 255
+
+X_mean = np.mean(X_train , axis=0)
+X_std = np.std(X_train , axis=0)
+X_std[X_std == 0.0] = 1.0
+X_inv_std=1.0 / X_std
+
+X_train = (X_train - X_mean) * X_inv_std
+X_test = (X_test - X_mean) * X_inv_std
+
 print('X_train shape:', X_train.shape)
+print('num classes:', nb_classes)
 print(X_train.shape[0], 'train samples')
 print(X_test.shape[0], 'test samples')
 
+input_shape = X_train.shape[1:]
+
 # convert class vectors to binary class matrices
-Y_train = np_utils.to_categorical(y_train, nb_classes)
-Y_test = np_utils.to_categorical(y_test, nb_classes)
+if nb_classes > 2:
+    Y_train = np_utils.to_categorical(Y_train, nb_classes)
+    Y_test = np_utils.to_categorical(Y_test, nb_classes)
 
 model = Sequential()
 
-model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1],
-                        border_mode='valid',
-                        input_shape=input_shape))
-model.add(Activation('relu'))
-model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=pool_size))
-model.add(Dropout(0.25))
+# model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1],
+#                         border_mode='valid',
+#                         input_shape=input_shape))
+# model.add(Activation('relu'))
+# model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
+# model.add(Activation('relu'))
+# model.add(MaxPooling2D(pool_size=pool_size))
+# model.add(Dropout(0.25))
 
-model.add(Flatten())
-model.add(Dense(128))
+# model.add(Flatten())
+model.add(Dense(128, input_shape=input_shape, W_regularizer=l2(0.01)))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
-model.add(Dense(nb_classes))
-model.add(Activation('softmax'))
+model.add(Dense(128, W_regularizer=l2(0.01)))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(128, W_regularizer=l2(0.01)))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+if nb_classes > 2:
+    model.add(Dense(nb_classes, W_regularizer=l2(0.01)))
+    model.add(Activation('softmax'))
+else:
+    model.add(Dense(1, W_regularizer=l2(0.01)))
+    model.add(Activation('sigmoid'))
 
-model.compile(loss=macro_avg_fm_loss(beta=1.0),
+model.compile(loss=macro_avg_fm_loss(beta=1.0), # 'binary_crossentropy',
               optimizer='adadelta',
-              metrics=['accuracy'])
+              metrics=['accuracy', 'fbeta_score'])
 
-model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
-          verbose=1, validation_data=(X_test, Y_test))
+#early_stopping = EarlyStopping(monitor='val_loss', min_delta=0,
+#                               patience=10, verbose=1, mode='min')
+
+model.fit(X_train, Y_train, batch_size=batch_size,
+          nb_epoch=nb_epoch, verbose=1, validation_split=0.3)
+
+# callbacks=[early_stopping])
+# validation_data=(X_test, Y_test)
+
 score = model.evaluate(X_test, Y_test, verbose=0)
-print('Test score:', score[0])
-print('Test accuracy:', score[1])
+print('Test scores:', score)
